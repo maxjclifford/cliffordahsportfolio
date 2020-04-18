1551020fd7842cac7afa4d5cacb0407c6548c587
gameplayer33 <41532188+gameplayer33@users.noreply.github.com>
2020-04-18 13:47:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+//Loads the header and footer into the proper positions. This definitely works, regardless of what the compiler says.
+$(document).ready(function () {
+    //var rellax = new Rellax('.rellax');
+    $("#navbar").load("header.html");
+});
